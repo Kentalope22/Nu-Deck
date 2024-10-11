f828651ad77a8d76b32f902bdabac74b489f3560
Kentalope22 <69648019+Kentalope22@users.noreply.github.com>
2024-10-10 23:12:29 -0700

    OptionsMenu(Beta0.2)
    -Added text for explanation and regions for organization
    -Press escape to open Pause Menu
    -No mouse needed to navigate
    -Able to switch between Pause and Party Menu

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -6,21 +6,25 @@ using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
+    //canvas screens to be activated and deactivated
+    [Header("Menu Objects")]
     [SerializeField] private GameObject _menuCanvasGO;
     [SerializeField] private GameObject _partyCanvasGO;
 
+    //objs for default first selected in each menu
+    [Header("Defaule First Selected")]
     [SerializeField] private GameObject _menuFirst;
     [SerializeField] private GameObject _partyFirst;
 
     private bool isPaused;
-
+    //set canvases to inactive at game start
     private void Start()
     {
         _menuCanvasGO.SetActive(false); 
         _partyCanvasGO.SetActive(false);    
 
     }
-
+    //checking for open menu input(escapekey for keyboard)
     private void Update()
     {
         if (InputManager.instance.MenuOpenCloseInput)
@@ -37,11 +41,14 @@ public class MenuManager : MonoBehaviour
         
     }
 
+    #region Pause/Unpause Functions
+    //pauses game, sets ingame tic speed to 0: DOES NOT pause user inputted animations
     public void Pause()
     {
         isPaused = true;
         Time.timeScale = 0f;
         OpenMenu();
+        //add line to pause user inputted animations
     }
 
     public void Unpause()
@@ -49,9 +56,11 @@ public class MenuManager : MonoBehaviour
         isPaused = false;
         Time.timeScale = 1f;
         CloseAllMenus();
-
+        //add line to unpause user inputted animations
     }
+    #endregion
 
+    #region Canvas Activation/Deactivation
     private void OpenMenu()
     {
         _menuCanvasGO.SetActive(true);
@@ -76,7 +85,9 @@ public class MenuManager : MonoBehaviour
 
         EventSystem.current.SetSelectedGameObject(null);
     }
+    #endregion
 
+    #region Menu Button Actions
     public void OnResumePress()
     {
         Unpause();
@@ -91,4 +102,5 @@ public class MenuManager : MonoBehaviour
     {
         OpenMenu();
     }
+    #endregion
 }
