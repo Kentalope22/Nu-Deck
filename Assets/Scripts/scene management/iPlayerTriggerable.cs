780f6b10670c0a0b9901871071ff2f93ed45b98f
Pragya-sharma24 <Pragya.sharma@csu.fullerton.edu>
2024-10-27 00:39:48 -0700

    changing scenes
    changing scenes from the main screen to the battle screen

--- Assets/Scripts/scene management/iPlayerTriggerable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface iPlayerTriggerable 
-{
-    void onPlayerTriggered(PlayerController player);
-    
-}
